5a380ec74984a6179689b8c50ff08f489523e379
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-06-26 18:09:43 +0530

    feat: frontend modifications for the users list

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
@@ -43,9 +45,12 @@ function outputMessage(message) {
 }
 
 function outputRoomName(room) {
-
+  roomName.innerText = room;
 }
 
+// Add users to DOM
 function outputUsers(users) {
-
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
 }
