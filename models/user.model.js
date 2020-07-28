e6946d244d435a676dba0ad08bb3b7c839aa2a00
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-29 00:01:21 +0530

    refactor: model restructured

--- models/user.model.js
+++ models/user.model.js
@@ -4,18 +4,18 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: {
-      type: String,
-      required: true,
-      unique: true
+        type: String,
+        required: true,
+        unique: true
     },
-    socketId: Number,
+    socketId: String,
     room: {
-      type: Schema.Types.ObjectId,
-      ref: 'Room'
+        type: String,
+        required: true
     }
   },
   {
-    timestamps: true
+      timestamps: true
   }
 );
 
